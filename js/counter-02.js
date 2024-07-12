6e2f37883c5b5807a2efbbaf0d2ea882a065714c
Olia <butskao@gmail.com>
2024-07-12 11:31:23 +0300

    перевірка чи є елемент кнопкою - та на товар який знаходиться в кошику, підключення до html

--- js/counter-02.js
+++ js/counter-02.js
@@ -12,4 +12,26 @@ window.addEventListener('click', function(event){
         counter = counterWrapper.querySelector('[data-counter]');  
     
         }
+
+        // чи є елемент кнопкою +
+   if (event.target.dataset.action === 'plus'){
+    counter.innerText = ++counter.innerText;
+}
+
+
+// чи є елемент кнопкою -
+if (event.target.dataset.action === 'minus'){
+ 
+//перевірка на товар який знаходиться в кошику   
+ if (parseInt(counter.innerText) > 1){
+     counter.innerText = --counter.innerText;
+ }else if(event.target.closest('.cart-wrapper') && parseInt(counter.innerText) === 1){
+     console.log('IN CART!!!!');
+
+     event.target.closest('.cart-item').remove();
+ }
+ 
+ 
+ 
+}
     });
