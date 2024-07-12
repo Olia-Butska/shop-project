650ee554a5ff9507583ff831583d5ed9cd5c7d23
NazarMaluga <nmaluga7@gmail.com>
2024-07-12 22:01:24 +0300

    Зібрані дані підставимо в шаблон для товару в кошику, відображення статусу кошика порожній/повний

--- js/cart-02.js
+++ js/cart-02.js
@@ -26,5 +26,46 @@ window.addEventListener('click', function(event){
  if (itemInCart){
   const counterElement = itemInCart.querySelector('[data-counter]');
   counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+}else{
+    //if not
+   
+      //зібрані дані підставимо в шаблон для товару в кошику
+      const cartItemHTML =`<div class="cart-item" data-id="${productInfo.id}">
+          <div class="cart-item__top">
+           <div class="cart-item__img">
+            <img src="${productInfo.imgSrc}" alt="${productInfo.title}">
+           </div>
+           <div class="cart-item__desc">
+            <div class="cart-item__title">${productInfo.title}</div>
+            <div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
+  
+            <!-- cart-item__details -->
+            <div class="cart-item__details">
+  
+             <div class="items items--small counter-wrapper">
+              <div class="items__control" data-action="minus">-</div>
+              <div class="items__current" data-counter="">${productInfo.counter}</div>
+              <div class="items__control" data-action="plus">+</div>
+             </div>
+  
+             <div class="price">
+              <div class="price__currency">${productInfo.price}</div>
+             </div>
+  
+            </div>
+            <!-- // cart-item__details -->
+  
+           </div>
+          </div>
+         </div>`;
+  
+      //відобразимо товар у кошику
+      cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+   }
+  
+   card.querySelector('[data-counter]').innerText = '1';
+      
+   //відображення статусу кошика порожній/повний
+   toggleCartStatus();
 }
 });
