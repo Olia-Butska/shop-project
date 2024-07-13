f81b80d5d76edc70823d76424789b0b81f361789
Olia <butskao@gmail.com>
2024-07-13 14:32:15 +0300

    звернення до властивості length класу cart-wrapper, що відображає замовлення в кошику, для показу кількості різних ролів в ньому

--- js/toggleCartStatus.js
+++ js/toggleCartStatus.js
@@ -1,3 +1,7 @@
 function toggleCartStatus (){
     console.log('toggleCartStatus');
+
+    const cartWrapper = document.querySelector('.cart-wrapper');
+    console.log(cartWrapper.children.length);
+
 }
